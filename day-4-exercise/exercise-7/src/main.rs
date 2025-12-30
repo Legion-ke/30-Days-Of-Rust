11bcb2d8fcdd91b023fb1cf30655a3ece3a74c57
Legion-ke <omondigiven@gmail.com>
2025-12-30 23:13:57 +0300

    all

--- day-4-exercise/exercise-7/src/main.rs
+++ day-4-exercise/exercise-7/src/main.rs
@@ -4,7 +4,7 @@ fn average_array(arr: &[i32]) -> i32 {
 }
 
 fn main() {
-    let number: [i32; 5] = [5, 10, 11, 200, 5];
+    let number: [i32; 7] = [5, 10, 11, 200, 15, 16, 50];
 
     let result = average_array(&number);
 
