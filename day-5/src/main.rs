fb6c24acad43b7b20a074892ddcf5459ae885760
Legion-ke <omondigiven@gmail.com>
2025-12-30 16:35:23 +0300

    removefrom day 5

--- day-5/src/main.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let day = 5;
-    println!("Hello, world this day {} of rust in the 30 days of rust", day);
-}
--- day-5/target/debug/day-5.d
+++
@@ -1,1 +0,0 @@
-/home/legacy/projects/30-Days-Of-Rust/day-5/target/debug/day-5: /home/legacy/projects/30-Days-Of-Rust/day-5/src/main.rs
